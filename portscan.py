d494f4da1daf10a9d6968eb178caffa06e471db0
hammadmemon456 <hammad1810e@aptechgdn.net>
2021-05-11 19:24:11 -0400

    Port Scaning Project

---
+++ portscan.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.setdefaulttimeout(2)
+
+host = raw_input("[*] Enter The Host To Scan: ")
+port = int(raw_input("[*] Enter The Port To Scan: "))
+
+def portscanner(port):
+	if sock.connect_ex((host,port)):
+		print "Port %d is closed" % (port)
+	else:
+		print "Port %d is open" % (port)
+
+portscanner(port)
